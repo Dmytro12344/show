df743b440f6d2c288ad8cc19959a8cb3a1e19f62
Dmytro12344 <dmytro.vasiuk@nure.ua>
2019-06-12 18:36:37 +0300

    created star page

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -5,13 +5,15 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class HomeController extends Controller
 {
-
-    public function show()
+    public function showAction(Request $request)
     {
+	$locale = $request->getLocale();
+//	dump($locale);die();
         $name = 'Dmytro';
         $title = 'Hello World';
         return $this->render('home.html.twig', ['name' => $name, 'title' => $title]);
